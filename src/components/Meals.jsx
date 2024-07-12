e1391a734824e058e41368b9785985f74171dae6
Your Name <urielparavi75@gmail.com>
2024-07-12 15:22:32 +0300

    the last branch

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,24 +1,25 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import useHttp from '../hooks/useHttp';
+import Error from './Error';
 import MealItem from './MealItem';
 
+const requestConfig = {};
+
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
+  // console.log(loadedMeals);
 
-      if (!response.ok) {
-        // ...
-      }
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
 
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-
-    fetchMeals();
-  }, []);
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
+  }
 
   return (
     <ul id="meals">
